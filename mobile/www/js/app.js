fb746b4864bec659089d2d6155e034196b02bb19
Brandon Borders <brandon@camelcased.com>
2015-10-07 15:52:28 -0700

    Setup buttons

--- mobile/www/js/app.js
+++ mobile/www/js/app.js
@@ -53,36 +53,14 @@ angular.module('starter', [
 
   // Each tab has its own nav history stack:
 
-  .state('tab.visualize', {
-    url: '/visualize',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-visualize.html',
-        controller: 'VisualizeController'
-      }
-    }
-  })
-  .state('tab.play', {
-    url: '/play',
-    views: {
-      'tab-play-sequence': {
-        templateUrl: 'templates/tab-play-sequence.html',
-        controller: 'PlayController'
-      }
-    }
-  })
-  .state('tab.check', {
-    url: '/check',
-    views: {
-      'tab-check-answer': {
-        templateUrl: 'templates/tab-check-answer.html',
-        controller: 'CheckAnswerController'
-      }
-    }
+  .state('game', {
+    url: '/game',
+    templateUrl: 'templates/tab-play-sequence.html',
+    controller: 'PlayController'
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/play');
+  $urlRouterProvider.otherwise('/game');
 
 })
 
